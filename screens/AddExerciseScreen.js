0ef58983d75c51841c743939cb7715444e43a9b5
Joh Yoshida <johyoshida@gmail.com>
2020-04-24 21:26:16 -0700

    Submit exercise to server database

--- screens/AddExerciseScreen.js
+++ screens/AddExerciseScreen.js
@@ -1,17 +1,54 @@
 import * as React from "react";
 import { Input } from "react-native-elements";
 import { Button, Container, Content, StyleProvider, Text } from "native-base";
+// Native base theme requirements
 import getTheme from "../native-base-theme/components";
 import platform from "../native-base-theme/variables/platform";
+
+const URL = "https://habitat-server.herokuapp.com";
 
 export default function AddExerciseScreen(props) {
   const [exercise, setExercise] = React.useState();
   const [errorTextExercise, setErrorTextExercise] = React.useState();
   const inputExercise = React.createRef();
 
-  const submit = () => {
-    props.navigation.goBack();
+  const submitExercise = () => {
+    const { exercises } = props.route.params;
+    console.log(exercises);
+    if (exercises.length === 0) {
+      postExercise();
+    } else {
+      let duplicate = false;
+      exercises.forEach(item => {
+        if (item.name === exercise) {
+          duplicate = true;
+          inputError();
+        }
+      });
+      if (!duplicate) postExercise();
+    }
   };
+
+  const postExercise = () => {
+    fetch(`${URL}/exercise`, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        name: exercise
+      })
+    })
+    .then(res => res.json())
+    .then(json => {
+      if (props.route.params.addExercise) {
+        props.route.params.addExercise(json.name, json.id);
+      }
+      props.route.params.getExercises();
+      props.navigation.goBack();
+    });
+  }
 
   const inputError = () => {
     inputExercise.current.shake();
@@ -34,10 +71,10 @@ export default function AddExerciseScreen(props) {
               setExercise(text);
             }}
           />
-          <Button block onPress={submit}>
+          <Button block onPress={submitExercise}>
             <Text>Submit</Text>
           </Button>
-          <Button block warning onPress={inputError.bind(this, "exercise")}>
+          <Button block warning onPress={inputError}>
             <Text>exercise error</Text>
           </Button>
         </Content>

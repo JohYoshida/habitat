88db2a5ff0ec49afd25e36bad5751809bb93ffbe
Joh Yoshida <johyoshida@gmail.com>
2020-04-25 13:03:42 -0700

    Fetch exercises and assemble/display list

--- screens/ExerciseScreen.js
+++ screens/ExerciseScreen.js
@@ -16,13 +16,42 @@ import Colors from "../constants/Colors";
 import getTheme from "../native-base-theme/components";
 import platform from "../native-base-theme/variables/platform";
 
+const URL = "https://habitat-server.herokuapp.com";
+
 export default function ExerciseScreen(props) {
+  const [exercises, setExercises] = React.useState([]);
+
+  const getExercises = () => {
+    fetch(`${URL}/exercises`, {
+      method: "GET"
+    })
+      .then(res => res.json())
+      .then(json => {
+        setExercises(json.data);
+      });
+  };
+
+  // Assemble exercise list
+  const ExercisesList = [];
+  exercises.forEach((exercise, index) => {
+    ExercisesList.push(
+      <ListItem
+        key={index}
+        onPress={() =>
+          props.navigation.navigate("View Exercise", { exercise, getExercises })
+        }
+      >
+        <Text>{exercise.name}</Text>
+      </ListItem>
+    );
+  });
 
   return (
     <StyleProvider style={getTheme(platform)}>
       <Container>
         <View style={styles.container}>
           <Content padder>
+            <List>{ExercisesList}</List>
           </Content>
 
           <ActionButton buttonColor={colors.brandPrimary}>

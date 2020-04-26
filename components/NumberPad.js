0b11a0db9b57dff387bd6a8687a965f46c1bce90
Joh Yoshida <johyoshida@gmail.com>
2020-04-26 03:10:56 -0700

    Prettier

--- components/NumberPad.js
+++ components/NumberPad.js
@@ -2,7 +2,7 @@ import * as React from "react";
 import { StyleSheet, View } from "react-native";
 import { Input } from "react-native-elements";
 import { Col, Row, Grid } from "react-native-easy-grid";
-import { Button, Container, StyleProvider, Text, H1, H2, H3 } from "native-base";
+import { Button, Container, StyleProvider, Text, H3 } from "native-base";
 import Colors from "../constants/Colors";
 // Native base theme requirements
 import getTheme from "../native-base-theme/components";
@@ -10,21 +10,23 @@ import platform from "../native-base-theme/variables/platform";
 
 export default function NumberPad(props) {
   let { initialValue } = props;
-  initialValue ? initialValue : "000000"
+  initialValue ? initialValue : "000000";
   const [hours, setHours] = React.useState(initialValue.slice(0, 2));
   const [minutes, setMinutes] = React.useState(initialValue.slice(2, 4));
   const [seconds, setSeconds] = React.useState(initialValue.slice(4, 6));
-  const [amount, setAmount] = React.useState(Number(initialValue) !== 0 ? Number(initialValue).toString() : "");
+  const [amount, setAmount] = React.useState(
+    Number(initialValue) !== 0 ? Number(initialValue).toString() : ""
+  );
 
   const updateAmount = value => {
     let num; // Up to 6 digits. No leading zeroes
     let str; // Will always be 6 digits, including any leading zeroes
-    if (value === "back")  {
+    if (value === "back") {
       num = amount.slice(0, -1);
-      str = "000000".slice(0, - amount.length + 1) + num;
+      str = "000000".slice(0, -amount.length + 1) + num;
     } else {
       num = Number(amount + value);
-      str = "000000".slice(0, - amount.length - 1) + num;
+      str = "000000".slice(0, -amount.length - 1) + num;
     }
     str += "000000";
     str = str.slice(0, 6);
@@ -33,7 +35,7 @@ export default function NumberPad(props) {
     setSeconds(str.slice(4, 6));
     setAmount(num.toString().slice(0, 6));
     props.callback(str);
-  }
+  };
 
   return (
     <View>
@@ -41,7 +43,9 @@ export default function NumberPad(props) {
         <Row>
           <Col>
             <Button transparent block>
-              <H3 style={styles.numberDisplay}>{hours} h {minutes} m {seconds} s</H3>
+              <H3 style={styles.numberDisplay}>
+                {hours} h {minutes} m {seconds} s
+              </H3>
             </Button>
           </Col>
           <Col>
@@ -109,13 +113,12 @@ export default function NumberPad(props) {
           </Col>
         </Row>
       </Grid>
-
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   numberDisplay: {
-    color: Colors.brandPrimary,
+    color: Colors.brandPrimary
   }
 });

f672f586439c1b2b543051d2b755fe09a4b10254
Joh Yoshida <johyoshida@gmail.com>
2020-04-30 16:56:36 -0700

    Implemented CustomButtons component to replace ButtonGroup from react-native-elements

---
+++ components/CustomButtons.js
@@ -0,0 +1,35 @@
+import * as React from "react";
+import { View } from "react-native";
+import { Col, Row, Grid } from "react-native-easy-grid";
+import { Button, Text } from "native-base";
+
+export default function CustomButtons(props) {
+  const Buttons = [];
+  props.buttons.forEach((label, index) => {
+    let custom = false;
+    if (label === "custom") custom = true;
+    if (props.selectedIndex === index) {
+      Buttons.push(
+        <Col style={custom ? { width: 100 } : {}}>
+          <Button block onPress={() => props.onPress(index)}>
+            <Text>{label}</Text>
+          </Button>
+        </Col>
+      );
+    } else {
+      Buttons.push(
+        <Col style={custom ? { width: 100 } : null}>
+          <Button bordered block onPress={() => props.onPress(index)}>
+            <Text>{label}</Text>
+          </Button>
+        </Col>
+      );
+    }
+  });
+
+  return (
+    <View style={props.style}>
+      <Grid>{Buttons}</Grid>
+    </View>
+  );
+}

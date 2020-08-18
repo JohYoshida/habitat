734f7240b22dd8b50e30641c72c447e6c3ec1cc9
Joh Yoshida <johyoshida@gmail.com>
2020-08-18 14:31:16 -0700

    Refactored graphs into charts and moved a bunch of code to viewExerciseScreenHelpers

---
+++ functions/viewExerciseScreenHelpers.js
@@ -0,0 +1,287 @@
+import * as React from "react";
+import {ListItem, Input} from "react-native-elements";
+
+const moment = require("moment");
+
+function assembleChartData(workouts) {
+  workouts.reverse();
+  const charts = {
+    // bar chart or goal progress bar?
+    today: {
+      data: [],
+      dates: [],
+      total: 0
+    },
+    // bar chart
+    thisWeek: {
+      data: [],
+      dates: [],
+      total: 0
+    },
+    // bar chart
+    thisMonth: {
+      data: [],
+      dates: [],
+      total: 0
+    },
+    // bar chart
+    lifetime: {
+      data: [],
+      dates: [],
+      total: 0
+    },
+    // line chart
+    cumulative: {
+      data: [],
+      total: 0
+    },
+    workoutsList: []
+  };
+
+  const todayList = ["Today"];
+  const thisWeekList = ["This Week"];
+  const thisMonthList = ["This Month"];
+
+  workouts.forEach((workout, index) => {
+    // Calculate difference in days between workout and now
+    const diff = moment()
+      .startOf("day")
+      .diff(moment(workout.createdAt).startOf("day"), "days");
+    let amount = calculateAmount(workout);
+
+    // Insert data for lifetime chart
+    charts.lifetime.total += amount;
+    if (!charts.lifetime.data[diff]) {
+      charts.lifetime.data[diff] = amount;
+      charts.lifetime.dates[diff] = moment(workout.createdAt).format(
+        "MM-DD-YYYY"
+      );
+    } else {
+      charts.lifetime.data[diff] += amount;
+    }
+
+    // Insert data for today, thisWeek, thisMonth charts and lists
+    if (diff == 0) {
+      // Today
+      charts.today.total += amount;
+      charts.today.data.push(amount);
+      let time = moment(workout.createdAt).format("hh:mm");
+      charts.today.dates.push(time);
+      // Add to list
+      todayList.push(workout);
+    }
+    if (diff < 7) {
+      // This week
+      charts.thisWeek.total += amount;
+      let data = charts.thisWeek.data;
+      if (!data[diff]) {
+        data[diff] = amount;
+        let date = moment(workout.createdAt).format("MMM Do");
+        charts.thisWeek.dates[diff] = date;
+      } else {
+        data[diff] += amount;
+      }
+      // Add to list
+      if (diff != 0) {
+        thisWeekList.push(workout);
+      }
+    }
+    if (diff < 30) {
+      // This month
+      charts.thisMonth.total += amount;
+      let data = charts.thisMonth.data;
+      if (!data[diff]) {
+        data[diff] = amount;
+        let date = moment(workout.createdAt).format("MMM Do");
+        charts.thisMonth.dates[diff] = date;
+      } else {
+        data[diff] += amount;
+      }
+
+      if (diff >= 7) {
+        thisMonthList.push(workout);
+      }
+    }
+  });
+
+  // Replace nulls with zeroes
+  cleanup(charts.cumulative);
+  cleanup(charts.lifetime, "MM-DD-YYYY");
+  cleanup(charts.thisWeek, "MMM Do");
+  cleanup(charts.thisMonth, "MMM Do");
+
+  // Reverse order of data
+  charts.today.data.reverse();
+  charts.thisWeek.data.reverse();
+  charts.thisWeek.dates.reverse();
+  charts.thisMonth.data.reverse();
+  charts.thisMonth.dates.reverse();
+  charts.lifetime.data.reverse();
+  charts.lifetime.dates.reverse();
+
+  // Insert data for cumulative chart
+  let cumulativeData = charts.cumulative.data;
+  charts.lifetime.data.forEach((value, index) => {
+    charts.cumulative.total = charts.lifetime.total;
+    if (!cumulativeData[index]) {
+      if (cumulativeData[index - 1]) {
+        cumulativeData[index] = cumulativeData[index - 1] + value;
+      } else {
+        cumulativeData[index] = value;
+      }
+    } else {
+      cumulativeData[index] += value;
+    }
+  });
+
+  // Concatenate workout lists
+  charts.workoutsList = todayList.concat(thisWeekList, thisMonthList);
+  // Add cumulative marker
+  charts.workoutsList.push("Cumulative");
+  return charts;
+}
+
+// Calculate amount
+function calculateAmount(workout) {
+  let amount;
+  if (workout.seconds) {
+    amount = workout.seconds;
+  } else {
+    amount = workout.reps * workout.sets;
+  }
+  return amount;
+}
+
+// Remove null values and replace with zeroes or dates
+function cleanup(dataset, format) {
+  for (var i = 0; i < dataset.data.length; i++) {
+    if (!dataset.data[i]) {
+      dataset.data[i] = 0;
+      if (dataset.dates) {
+        dataset.dates[i] = moment()
+        .subtract(i, "days")
+        .format(format);;
+      }
+    }
+  }
+}
+
+function assembleWorkoutsList(chartData, name, mode, displayChart, setDisplayChart, workoutDeleteID, setWorkoutDeleteID) {
+  const WorkoutsList = [];
+  chartData.workoutsList.forEach(item => {
+    if (item == "Today") {
+      let title = assembleTitle(mode, chartData.today.total, name);
+      WorkoutsList.push(
+        <ListItem
+          key="today-header"
+          title="Today"
+          topDivider={true}
+          bottomDivider={true}
+          rightTitle={title}
+          onPress={() => {
+            if (displayChart === "today") {
+              setDisplayChart(null);
+            } else {
+              setDisplayChart("today");
+            }
+          }}
+        />
+      );
+    } else if (item == "This Week") {
+      let title = assembleTitle(mode, chartData.thisWeek.total, name);
+      WorkoutsList.push(
+        <ListItem
+          key="thisWeek-header"
+          title="This Week"
+          topDivider={true}
+          bottomDivider={true}
+          rightTitle={title}
+          onPress={() => {
+            if (displayChart === "thisWeek") {
+              setDisplayChart(null);
+            } else {
+              setDisplayChart("thisWeek");
+            }
+          }}
+        />
+      );
+    } else if (item == "This Month") {
+      let title = assembleTitle(mode, chartData.thisMonth.total, name);
+      WorkoutsList.push(
+        <ListItem
+          key="thisMonth-header"
+          title="This Month"
+          topDivider={true}
+          bottomDivider={true}
+          rightTitle={title}
+          onPress={() => {
+            if (displayChart === "thisMonth") {
+              setDisplayChart(null);
+            } else {
+              setDisplayChart("thisMonth");
+            }
+          }}
+        />
+      );
+    } else if (item == "Cumulative") {
+      let title = assembleTitle(mode, chartData.cumulative.total, name);
+      WorkoutsList.push(
+        <ListItem
+          key="cumulative-header"
+          title="Cumulative"
+          topDivider={true}
+          bottomDivider={true}
+          rightTitle={title}
+          onPress={() => {
+            if (displayChart === "cumulative") {
+              setDisplayChart(null);
+            } else {
+              setDisplayChart("cumulative");
+            }
+          }}
+        />
+      );
+    } else {
+      let title;
+      let timestamp = moment(item.createdAt).format("h:mm a MM-DD-YYYY");
+      if (mode === "time") title = assembleTitle(mode, item.seconds, name);
+      else title = assembleTitle(mode, item.reps * item.sets, name);
+      WorkoutsList.push(
+        <ListItem
+          key={item.id}
+          title={
+            mode === "time"
+              ? `${item.seconds} seconds`
+              : `${item.reps} reps, ${item.sets} sets`
+          }
+          rightTitle={title}
+          subtitle={timestamp}
+          onPress={() => {
+            if (item.id === workoutDeleteID) {
+              setWorkoutDeleteID(null);
+            } else {
+              setWorkoutDeleteID(item.id);
+            }
+          }}
+        />
+      );
+    }
+  });
+
+  return WorkoutsList;
+}
+
+const assembleTitle = (mode, number, name) => {
+  let title;
+  if (mode === "time") {
+    let hours = Math.floor(number / 3600);
+    let minutes = Math.floor((number - hours * 3600) / 60);
+    let seconds = Math.floor(number - hours * 3600 - minutes * 60);
+    title = `${hours} h ${minutes} m ${seconds} s`;
+  } else if (mode === "reps and sets") {
+    title = `${number} ${name}`;
+  }
+  return title;
+};
+
+export {assembleChartData, assembleWorkoutsList};
--- screens/ViewExerciseScreen.js
+++ screens/ViewExerciseScreen.js
@@ -10,7 +10,7 @@ import {
   Text
 } from "native-base";
 import * as Haptics from 'expo-haptics';
-import { LineChart, Grid, XAxis, YAxis } from "react-native-svg-charts";
+import { BarChart, LineChart, Grid, XAxis, YAxis } from "react-native-svg-charts";
 import * as shape from "d3-shape";
 import { ConfirmDeletionButtons } from "../components/ConfirmDeletionButtons";
 import {
@@ -19,6 +19,7 @@ import {
   deleteExercise,
   deleteWorkout
 } from "../functions/fetch";
+import { assembleChartData, assembleWorkoutsList } from "../functions/viewExerciseScreenHelpers";
 import Colors from "../constants/Colors";
 // Native base theme requirements
 import getTheme from "../native-base-theme/components";
@@ -44,17 +45,16 @@ export default function ViewExerciseScreen(props) {
     Number(props.route.params.exercise.lifetimeTotal)
   );
 
-  // Hooks for list
-  const [listData, setListData] = React.useState([]);
-
-  // Hooks for graph
-  const [graphData, setGraphData] = React.useState({
-    main: { data: [], dates: [] },
-    today: { data: [], dates: [] },
-    yesterday: { data: [], dates: [] },
-    thisWeek: { data: [], dates: [] }
+  // Hooks for chart
+  const [chartData, setChartData] = React.useState({
+    today: { data: [], dates: [], total: 0 },
+    thisWeek: { data: [], dates: [], total: 0 },
+    thisMonth: { data: [], dates: [], total: 0 },
+    lifetime: { data: [], dates: [], total: 0 },
+    cumulative: { data: [], total: 0 },
+    workoutsList: []
   });
-  const [displayGraph, setDisplayGraph] = React.useState(null);
+  const [displayChart, setDisplayChart] = React.useState(null);
 
   // Get workouts when the screen mounts or state updates
   React.useEffect(
@@ -73,7 +73,7 @@ export default function ViewExerciseScreen(props) {
       // Get and set workouts from database
       fetchWorkouts(exercise_id).then(data => {
         setWorkouts(data);
-        assembleData(data);
+        setChartData(assembleChartData(data));
         setRefreshing(false);
       });
     },
@@ -123,250 +123,11 @@ export default function ViewExerciseScreen(props) {
     });
   };
 
-  // Assemble data for lists and graphs
-  const assembleData = workouts => {
-    const main = {
-      data: [],
-      dates: []
-    };
-    const today = {
-      data: [],
-      cumulative: [],
-      dates: []
-    };
-    const yesterday = {
-      data: [],
-      cumulative: [],
-      dates: []
-    };
-    const thisWeek = {
-      data: [],
-      cumulative: [],
-      dates: []
-    };
-    const lifetime = {
-      data: [],
-      cumulative: [],
-      dates: []
-    };
-    let workoutList = [];
-    const todayList = [];
-    const yesterdayList = [];
-    const thisWeekList = [];
-    const name = props.route.params.exercise.name.toLowerCase();
-    const mode = props.route.params.exercise.mode;
-    let place = "today";
-    workouts.forEach((workout, index) => {
-      let diff = moment()
-        .startOf("day")
-        .diff(moment(workout.createdAt).startOf("day"), "days");
-      let timestamp = moment(workout.createdAt).format("h:mm a MM-DD-YYYY");
-      // Calculate amount
-      let amount;
-      if (workout.seconds) {
-        amount = workout.seconds;
-      } else {
-        amount = workout.reps * workout.sets;
-      }
-      // Insert data into graphData main array
-      if (!main.data[diff]) {
-        main.data[diff] = amount;
-        main.dates[diff] = moment(workout.createdAt).format("MM-DD-YYYY");
-      } else {
-        main.data[diff] += amount;
-      }
-      // Insert data into graphData secondary arrays
-      let title;
-      if (diff === 0) {
-        // Today
-        today.data[index] = amount;
-        today.cumulative[index - 1]
-          ? (today.cumulative[index] = amount + today.cumulative[index - 1])
-          : (today.cumulative[index] = amount);
-        today.dates[index] = moment(workout.createdAt).format("hh:mm");
-        todayList.push(workout);
-      } else if (diff === 1) {
-        // Yesterday
-        yesterday.data[index] = amount;
-        yesterday.cumulative[index - 1]
-          ? (yesterday.cumulative[index] =
-              amount + yesterday.cumulative[index - 1])
-          : (yesterday.cumulative[index] = amount);
-        yesterday.dates[index] = moment(workout.createdAt).format("hh:mm");
-        yesterdayList.push(workout);
-      } else if (diff < 7) {
-        thisWeekList.push(workout);
-      }
-      if (diff < 7) {
-        // This week
-        thisWeek.data[index] = amount;
-        thisWeek.cumulative[index - 1]
-          ? (thisWeek.cumulative[index] =
-              amount + thisWeek.cumulative[index - 1])
-          : (thisWeek.cumulative[index] = amount);
-        thisWeek.dates[index] = moment(workout.createdAt).format("MM-DD-YYYY");
-      }
-      // Lifetime
-      lifetime.data[index] = amount;
-      lifetime.cumulative[index - 1]
-        ? (lifetime.cumulative[index] = amount + lifetime.cumulative[index - 1])
-        : (lifetime.cumulative[index] = amount);
-      lifetime.dates[index] = moment(workout.createdAt).format("MM-DD-YYYY");
-    });
-    for (var i = 0; i < main.data.length; i++) {
-      if (!main.data[i]) {
-        main.data[i] = 0;
-        main.dates[i] = moment()
-          .subtract(i, "days")
-          .format("MM-DD-YY");
-      }
-    }
-    // Reverse main graph datasets
-    main.data.reverse();
-    main.dates.reverse();
-    let finalData = { main, today, yesterday, thisWeek, lifetime };
-    // Filter out undefined entries
-    for (var array in finalData) {
-      if (finalData.hasOwnProperty(array)) {
-        for (var list in finalData[array]) {
-          if (finalData[array].hasOwnProperty(list)) {
-            finalData[array][list] = finalData[array][list].filter(n => {
-              if (n === 0) {
-                return true;
-              } else {
-                return n;
-              }
-            });
-          }
-        }
-      }
-    }
-    // Add titles to lists
-    todayList.push("Today");
-    yesterdayList.push("Yesterday");
-    thisWeekList.push("This Week");
-    // Combine lists
-    workoutList = [].concat(yesterdayList.reverse(), thisWeekList.reverse());
-    workoutList = [].concat(todayList.reverse(), workoutList);
-    workoutList.push("Lifetime");
-    // Update state
-    setGraphData(finalData);
-    setListData(workoutList);
-  };
 
   // Assemble workouts list
-  const WorkoutsList = [];
   const name = props.route.params.exercise.name.toLowerCase();
   const mode = props.route.params.exercise.mode;
-  listData.forEach((item, index) => {
-    let timestamp = moment(item.createdAt).format("h:mm a MM-DD-YYYY");
-    let title;
-    if (item === "Today") {
-      let todayTitle = assembleTitle(
-        mode,
-        graphData.main.data.slice().reverse()[0],
-        name
-      );
-      WorkoutsList.push(
-        <ListItem
-          key="today"
-          title="Today"
-          topDivider={true}
-          bottomDivider={true}
-          rightTitle={todayTitle}
-          onPress={() => {
-            if (displayGraph === "today") {
-              setDisplayGraph(null);
-            } else {
-              setDisplayGraph("today");
-            }
-          }}
-        />
-      );
-    } else if (item === "Yesterday") {
-      let yesterdayTitle = assembleTitle(
-        mode,
-        graphData.main.data.slice().reverse()[1],
-        name
-      );
-      WorkoutsList.push(
-        <ListItem
-          key="yesterday"
-          title="Yesterday"
-          topDivider={true}
-          bottomDivider={true}
-          rightTitle={yesterdayTitle}
-          onPress={() => {
-            if (displayGraph === "yesterday") {
-              setDisplayGraph(null);
-            } else {
-              setDisplayGraph("yesterday");
-            }
-          }}
-        />
-      );
-    } else if (item === "This Week") {
-      let thisWeekTotal = graphData.thisWeek.data.reduce((a, b) => a + b, 0);
-      let thisWeekTitle = assembleTitle(mode, thisWeekTotal, name);
-      WorkoutsList.push(
-        <ListItem
-          key="thisWeek"
-          title="This Week"
-          topDivider={true}
-          bottomDivider={true}
-          rightTitle={thisWeekTitle}
-          onPress={() => {
-            if (displayGraph === "thisWeek") {
-              setDisplayGraph(null);
-            } else {
-              setDisplayGraph("thisWeek");
-            }
-          }}
-        />
-      );
-    } else if (item === "Lifetime") {
-      let lifetimeTotal_2 = graphData.lifetime.data.reduce((a, b) => a + b, 0);
-      let lifetimeTitle = assembleTitle(mode, lifetimeTotal_2, name);
-      WorkoutsList.push(
-        <ListItem
-          key="lifetime"
-          title="Lifetime"
-          topDivider={true}
-          bottomDivider={true}
-          rightTitle={lifetimeTitle}
-          onPress={() => {
-            if (displayGraph === "lifetime") {
-              setDisplayGraph(null);
-            } else {
-              setDisplayGraph("lifetime");
-            }
-          }}
-        />
-      );
-    } else {
-      if (mode === "time") title = assembleTitle(mode, item.seconds, name);
-      else title = assembleTitle(mode, item.reps * item.sets, name);
-      WorkoutsList.push(
-        <ListItem
-          key={item.id}
-          title={
-            mode === "time"
-              ? `${item.seconds} seconds`
-              : `${item.reps} reps, ${item.sets} sets`
-          }
-          rightTitle={title}
-          subtitle={timestamp}
-          onPress={() => {
-            if (item.id === workoutDeleteID) {
-              setWorkoutDeleteID(null);
-            } else {
-              setWorkoutDeleteID(item.id);
-            }
-          }}
-        />
-      );
-    }
-  });
+  const WorkoutsList = assembleWorkoutsList(chartData, name, mode, displayChart, setDisplayChart, workoutDeleteID, setWorkoutDeleteID);
 
   // Conditionally display workouts list, empty list text, or loading spinner
   let ListDisplay;
@@ -376,7 +137,6 @@ export default function ViewExerciseScreen(props) {
         workouts you add will appear here
       </Text>
     );
-    // } else ListDisplay = WorkoutsList;
   } else ListDisplay = WorkoutsList;
 
   // Conditional rendering for workout delete/confirm buttons
@@ -409,7 +169,7 @@ export default function ViewExerciseScreen(props) {
     );
   }
 
-  // Conditionally render workout delete buttons in workout list
+  // Conditionally render workout delete buttons in workout list2
   if (workoutDeleteID !== null) {
     WorkoutsList.forEach((object, index) => {
       if (object && object.key === workoutDeleteID) {
@@ -444,19 +204,24 @@ export default function ViewExerciseScreen(props) {
   }
 
   // Make main graph for display
-  const MainGraph = makeGraph(
-    graphData.main.data,
-    graphData.main.dates,
-    "main"
+  const LifetimeChart = makeBarChart(
+    chartData.lifetime.data,
+    chartData.lifetime.dates,
+    "lifetime"
   );
 
-  // Conditionally render secondary graphs in WorkoutsList
-  if (displayGraph !== null) {
-    let { cumulative, dates } = graphData[displayGraph];
-    let graph = makeGraph(cumulative, dates, displayGraph);
+  if (displayChart !== null) {
+    let chart;
+    if (displayChart == "cumulative") {
+      // line chart
+      chart = makeLineChart(chartData.cumulative.data, chartData.lifetime.dates, displayChart);
+    } else {
+      // bar chart
+      chart = makeBarChart(chartData[displayChart].data, chartData[displayChart].dates, displayChart)
+    }
     WorkoutsList.forEach((object, index) => {
-      if (object && object.key === displayGraph) {
-        WorkoutsList.splice(index + 1, 0, graph).join();
+      if (object && object.key == `${displayChart}-header`) {
+        WorkoutsList.splice(index + 1, 0, chart).join();
       }
     });
   }
@@ -483,8 +248,8 @@ export default function ViewExerciseScreen(props) {
             <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
           }
         >
-          {MainGraph}
-          {ListDisplay}
+          {LifetimeChart}
+          {WorkoutsList}
           <View style={styles.buttons}>{DeleteExerciseButtons}</View>
         </Content>
       </Container>
@@ -506,8 +271,7 @@ const assembleTitle = (mode, number, name) => {
   return title;
 };
 
-// Make custom graph
-const makeGraph = (data, XAxisData, key) => {
+const makeBarChart = (data, XAxisData, key) => {
   if (data && data.length > 1) {
     return (
       <View
@@ -520,6 +284,53 @@ const makeGraph = (data, XAxisData, key) => {
           contentInset={{ top: 10, bottom: 10 }}
           svg={{ fontSize: 10, fill: "grey" }}
           numberOfTicks={6}
+          min={0}
+        />
+        <View style={{ flex: 1, marginLeft: 10 }}>
+          <BarChart
+            style={{ flex: 1 }}
+            data={data}
+            contentInset={{ top: 10, bottom: 10 }}
+            svg={{ fill: "#2196F3"}}
+            gridMin={0}
+          >
+            <Grid />
+          </BarChart>
+          <XAxis
+            style={{ marginHorizontal: -10, height: 30 }}
+            data={XAxisData}
+            contentInset={{ left: 10, right: 10 }}
+            svg={{ fontSize: 10, fill: "grey" }}
+            formatLabel={(value, index) => XAxisData[index]}
+            numberOfTicks={4}
+          />
+        </View>
+      </View>
+    );
+  } else {
+    return (
+      <Text key="text" style={styles.emptyListText}>
+        add more workouts to see this graph
+      </Text>
+    );
+  }
+}
+
+// Make custom graph
+const makeLineChart = (data, XAxisData, key) => {
+  if (data && data.length > 1) {
+    return (
+      <View
+        style={{ height: 300, padding: 10, flexDirection: "row" }}
+        key={`${key} graph`}
+      >
+        <YAxis
+          data={data}
+          style={{ marginBottom: 30 }}
+          contentInset={{ top: 10, bottom: 10 }}
+          svg={{ fontSize: 10, fill: "grey" }}
+          numberOfTicks={6}
+          min={0}
         />
         <View style={{ flex: 1, marginLeft: 10 }}>
           <LineChart
@@ -527,6 +338,7 @@ const makeGraph = (data, XAxisData, key) => {
             data={data}
             contentInset={{ top: 10, bottom: 10 }}
             svg={{ stroke: "#2196F3" }}
+            gridMin={0}
           >
             <Grid />
           </LineChart>

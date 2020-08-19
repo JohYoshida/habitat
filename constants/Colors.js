2508ffcb000a389d2cea15dccb15fc6ce042a716
Joh Yoshida <johyoshida@gmail.com>
2020-08-18 18:25:06 -0700

    Added brandSuccess color

--- constants/Colors.js
+++ constants/Colors.js
@@ -1,15 +1,16 @@
-const tintColor = '#2f95dc';
+const tintColor = "#2f95dc";
 
 export default {
   tintColor,
-  tabIconDefault: '#ccc',
+  tabIconDefault: "#ccc",
   tabIconSelected: tintColor,
-  tabBar: '#fefefe',
-  errorBackground: 'red',
-  errorText: '#fff',
-  warningBackground: '#EAEB5E',
-  warningText: '#666804',
+  tabBar: "#fefefe",
+  errorBackground: "red",
+  errorText: "#fff",
+  warningBackground: "#EAEB5E",
+  warningText: "#666804",
   noticeBackground: tintColor,
-  noticeText: '#fff',
-  brandPrimary: "#2196F3"
+  noticeText: "#fff",
+  brandPrimary: "#2196F3",
+  brandSuccess: "#4CAF50"
 };
